13ae559d26f8e3e8f2cf2512563509eb7daa13aa
Sepwizzz <115958805+Sepwizzz@users.noreply.github.com>
2022-12-14 08:33:46 -0500

    agrego los dos faltantes

---
+++ ciclos.py/buc1.py
@@ -0,0 +1,7 @@
+lista=["calcetin","pantalos","camisa","camiseta","otro calsetin","gorrra"]
+#creo lista 
+for prenda in lista: #penda en est caso sera un sapo que salta en posicion en pocicon 
+    
+    print("la lavadora se ha comido mi "+ prenda )
+
+#este codigo es mio
---
+++ ciclos.py/buc2.py
@@ -0,0 +1,9 @@
+for i in range(12,3,-3):# es inicion, fin, de acuanto #el for lo hace solo 
+    print(i)
+    i=0
+"""i=12
+while i >3: # aca el wile lo hace acumulando el valor de i lo suma asi que i cambia y en la proxima vale mas 
+    print(i)
+    i=i-3 #aca se le pone 3 para que cuente de tres en tres  """
+
+#este codigo es mio 
